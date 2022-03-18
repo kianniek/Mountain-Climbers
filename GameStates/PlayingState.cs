ebe44ecd4f1795e088debd015580621e99707ca6
luuk <luuk.van.es2@hva.nl>
2022-03-18 15:01:25 +0100

    Added playing state

---
+++ GameStates/PlayingState.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BaseProject.GameStates
+{
+    public class PlayingState : IGameLoopObject
+    {
+        public void HandleInput(InputHelper inputHelper)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
